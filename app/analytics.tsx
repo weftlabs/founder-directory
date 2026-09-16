a6da98da717c351d7ebfd6cbf5c3ba80b96b9d06
Patrick Barattin <p.barattin@gmail.com>
2026-09-16 18:47:42 +0200

    fix: send PostHog $pageleave on App Router nav and tab close (#19)
    Turning off automatic $pageview also starved $pageleave. Capture leave
    for the previous URL on client navigations and on pagehide.

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 
 type PostHog = {
@@ -26,10 +26,9 @@ function options() {
       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
     ui_host: "https://eu.posthog.com",
     person_profiles: "identified_only" as const,
-    // App Router never does a full reload; the SDK's automatic $pageview
-    // on init is easy to miss. We send it after navigation instead.
+    // App Router never full-reloads. Send both events ourselves.
     capture_pageview: false,
-    capture_pageleave: true,
+    capture_pageleave: false,
     disable_session_recording: true,
   };
 }
@@ -41,23 +40,43 @@ function install() {
   if (!key || typeof window === "undefined" || booted) return;
   if (!window.posthog?.__SV) {
     const snippet = document.createElement("script");
-    snippet.text = `!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once identify reset get_distinct_id".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);`;
+    snippet.text = `!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=\" (stub)\"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once identify reset get_distinct_id".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);`;
     document.head.appendChild(snippet);
   }
   window.posthog?.init(key, options());
   booted = true;
 }
 
+function send(event: "$pageview" | "$pageleave", url: string) {
+  window.posthog?.capture(event, { $current_url: url });
+}
+
 function PageViews() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const previousUrl = useRef<string | null>(null);
+
   useEffect(() => {
     if (!projectKey()) return;
     install();
-    window.posthog?.capture("$pageview", {
-      $current_url: window.location.href,
-    });
+    const url = window.location.href;
+    if (previousUrl.current && previousUrl.current !== url) {
+      send("$pageleave", previousUrl.current);
+    }
+    send("$pageview", url);
+    previousUrl.current = url;
   }, [pathname, searchParams]);
+
+  useEffect(() => {
+    if (!projectKey()) return;
+    const onHide = () => {
+      const url = previousUrl.current ?? window.location.href;
+      send("$pageleave", url);
+    };
+    window.addEventListener("pagehide", onHide);
+    return () => window.removeEventListener("pagehide", onHide);
+  }, []);
+
   return null;
 }
 
